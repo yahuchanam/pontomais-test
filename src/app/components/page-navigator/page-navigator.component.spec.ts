f3ddedfc60fd433bdf9a84da510e5a8570f52f46
Marcus Borges <yahuchanam@gmail.com>
2022-08-02 21:45:23 -0300

    feat: add page navigator component

---
+++ src/app/components/page-navigator/page-navigator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageNavigatorComponent } from './page-navigator.component';
+
+describe('PageNavigatorComponent', () => {
+  let component: PageNavigatorComponent;
+  let fixture: ComponentFixture<PageNavigatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageNavigatorComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PageNavigatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
