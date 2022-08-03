d745af83bcdee289d15f22244cf6c6433b207f87
Marcus Borges <yahuchanam@gmail.com>
2022-08-02 21:40:20 -0300

    chore: config env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  marvel: 'https://gateway.marvel.com'
+  marvel: 'https://gateway.marvel.com',
+  marvelPublicKey: 'db1f4c658a0e335c6712d269715d07bd',
+  marvelHash: '93aac1cabc4b589153bce63ffd77e86b'
 };
