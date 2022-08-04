1f5607847c146ea252f23ad386a022f38063ef17
Marcus Borges <yahuchanam@gmail.com>
2022-08-03 23:03:56 -0300

    feat: add pageSize config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,3 +3,4 @@ import { environment } from 'src/environments/environment';
 export const marvelAPI = environment.marvel;
 export const marvelPublicKey = environment.marvelPublicKey;
 export const marvelHash = environment.marvelHash;
+export const apiPageSize = environment.apiPageSize;
