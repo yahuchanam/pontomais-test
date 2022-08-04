e009f8412ca985ee3834c14ab13595dd4f8ce7a1
Marcus Borges <yahuchanam@gmail.com>
2022-08-03 23:09:26 -0300

    feat: add mobile listener to count pipe

--- src/app/pipes/count-number/count-number.pipe.ts
+++ src/app/pipes/count-number/count-number.pipe.ts
@@ -1,12 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { HostListener, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'countNumber',
 })
 export class CountNumberPipe implements PipeTransform {
+  maxPage = 3;
+
+  constructor() {
+    this.onResize(new Event('window:resize'));
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.maxPage = globalThis.innerWidth < 1024 ? 3 : 6;
+  }
 
   transform(value: number, selected: number = 1): number[] {
-    if (value < 3) return this.listOfLessMinimum(value);
+    if (value < this.maxPage) return this.listOfLessMinimum(value);
     if (selected === 1) return this.listInit(selected);
     if (selected + 1 < value) return this.list(selected);
     return this.listEnd(value);
@@ -22,19 +32,28 @@ export class CountNumberPipe implements PipeTransform {
 
   protected listInit(selected: number): number[] {
     const numbers: number[] = [];
-    for (let i = selected - 1; i < 3; i++) {
+    for (let i = selected - 1; i < this.maxPage; i++) {
       numbers.push(i + 1);
     }
     return numbers;
   }
 
   protected list(selected: number): number[] {
-    return [selected - 1, selected, selected + 1];
+    return this.maxPage === 3
+      ? [selected - 1, selected, selected + 1]
+      : [
+          selected - 1,
+          selected,
+          selected + 1,
+          selected + 2,
+          selected + 3,
+          selected + 4,
+        ];
   }
 
   protected listEnd(value: number): number[] {
     const numbers: number[] = [];
-    for (let i = value - 2; i <= value; i++) {
+    for (let i = value - (this.maxPage - 1); i <= value; i++) {
       numbers.push(i);
     }
     return numbers;
